5c2a20e5413b7dc91e501562b8493ab5cd1ac50b
NoorAzam-1 <azamnoor698@gmail.com>
2025-10-29 19:08:26 +0530

    all data added

---
+++ src/components/About/About.jsx
@@ -0,0 +1,104 @@
+import ReactTypingEffect from "react-typing-effect";
+import Tilt from "react-parallax-tilt";
+
+const About = () => {
+  const calculateMonths = () => {
+    const startDate = new Date("2025-03-01");
+    const currentDate = new Date();
+
+    let months =
+      currentDate.getMonth() -
+      startDate.getMonth() +
+      12 * (currentDate.getFullYear() - startDate.getFullYear());
+
+    return months;
+  };
+
+  const formatExperience = () => {
+    const totalMonths = calculateMonths();
+    const years = Math.floor(totalMonths / 12);
+    const months = totalMonths % 12;
+    
+    let experienceText = "";
+    
+    if (years > 0) {
+      experienceText += `${years} Year${years > 1 ? 's' : ''}`;
+    }
+    
+    if (months > 0) {
+      if (experienceText) experienceText += " ";
+      experienceText += `${months} Month${months > 1 ? 's' : ''}`;
+    }
+    
+    return experienceText;
+  };
+
+  return (
+    <section
+      id="about"
+      className="py-4 px-[7vw] md:px-[7vw] lg:px-[12vw] font-sans mt-10"
+    >
+      <div className="flex gap-2 flex-col-reverse lg:flex-row justify-between items-center max-w-7xl mx-auto">
+        <div className="lg:w-1/2 text-center lg:text-left mt-8 lg:mt-0">
+          <h1 className="text-3xl sm:text-4xl lg:text-3xl font-bold text-white mb-2 leading-tight ">
+            Hi, I am
+          </h1>
+          <h2 className="text-4xl sm:text-5xl lg:text-6xl font-bold text-white mb-4 leading-tight">
+            Noor Azam
+          </h2>
+          {/* Skills Heading with Typing Effect */}
+          <h3 className="text-xl sm:text-2xl lg:text-3xl font-semibold mb-4 text-[#8245ec] leading-tight">
+            <span className="text-white">I am a </span>
+            <ReactTypingEffect
+              text={["Full Stack Developer", "Web Developer", "Coder"]}
+              speed={100}
+              eraseSpeed={50}
+              typingDelay={500}
+              eraseDelay={2000}
+              cursorRenderer={(cursor) => (
+                <span className="text-[#8245ec]">{cursor}</span>
+              )}
+            />
+          </h3>
+          <p className="text-base sm:text-lg lg:text-lg text-gray-400 mb-10 mt-8 leading-relaxed">
+            I am a full-stack developer with over {formatExperience()} of
+            experience in building scalable web applications. Skilled in both
+            front-end and back-end development, I specialize in the MERN stack
+            and other modern technologies to create seamless user experiences
+            and efficient solutions.
+          </p>
+          <a
+            href="./NoorAzam.pdf"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
+            style={{
+              background: "linear-gradient(90deg, #8245ec, #a855f7)",
+              boxShadow: "0 0 2px #8245ec, 0 0 2px #8245ec, 0 0 40px #8245ec",
+            }}
+          >
+            DOWNLOAD CV
+          </a>
+        </div>
+        <div className="md:w-1/2 flex justify-center md:justify-end z-1 ">
+          <Tilt
+            className="w-60 h-60 sm:w-64 sm:h-64 md:w-[24rem] md:h-[20rem] lg:w-[30rem] lg:h-[30rem] border-4 border-purple-700 rounded-full"
+            tiltMaxAngleX={20}
+            tiltMaxAngleY={20}
+            perspective={1000}
+            scale={1.05}
+            transitionSpeed={1000}
+          >
+            <img
+              src="/myImage.png"
+              alt="Aman Kanojiya"
+              className="w-full h-full rounded-full object-cover top-10 drop-shadow-[0_10px_20px_rgba(130,69,236,0.5)]"
+            />
+          </Tilt>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
---
+++ src/constants.js
@@ -0,0 +1,232 @@
+import htmlLogo from './assets/tech_logo/html.png';
+import cssLogo from './assets/tech_logo/css.png';
+import javascriptLogo from './assets/tech_logo/javascript.png';
+import reactjsLogo from './assets/tech_logo/reactjs.png';
+import reduxLogo from './assets/tech_logo/redux.png';
+import nextjsLogo from './assets/tech_logo/nextjs.png';
+import tailwindcssLogo from './assets/tech_logo/tailwindcss.png';
+import materialuiLogo from './assets/tech_logo/materialui.png';
+import bootstrapLogo from './assets/tech_logo/bootstrap.png';
+import nodejsLogo from './assets/tech_logo/nodejs.png';
+import expressjsLogo from './assets/tech_logo/express.png';
+import mongodbLogo from './assets/tech_logo/mongodb.png';
+import typescriptLogo from './assets/tech_logo/typescript.png';
+import gitLogo from './assets/tech_logo/git.png';
+import githubLogo from './assets/tech_logo/github.png';
+import vscodeLogo from './assets/tech_logo/vscode.png';
+import postmanLogo from './assets/tech_logo/postman.png';
+import mcLogo from './assets/tech_logo/mc.png';
+import vercelLogo from './assets/tech_logo/vercel.png';
+import bcryptLogo from './assets/tech_logo/bcrypt.svg';
+import argonLogo from './assets/tech_logo/argonLogo.png';
+import cloudinaryLogo from './assets/tech_logo/cloudinaryLogo.webp';
+import jwtLogo from './assets/tech_logo/JWTLogo.png';
+import NodemailerLogo from './assets/tech_logo/Nodemailer.png';
+import ashatech from './assets/company_logo/ashatech.png';
+import AshaLearnology from './assets/work_logo/AshaLearnology.png';
+import AshaTech from './assets/work_logo/AshaTech.png';
+import WaveVisas from './assets/work_logo/WaveVisas.png';
+import fireCommerce from './assets/work_logo/fireCommerce.png';
+import excelAnalytics from "./assets/work_logo/excelAnalytics.png"
+import blogpulse from "./assets/work_logo/blogpulse.png"
+import schoolLogo from "./assets/education_logo/school.png"
+import ignouLogo from "./assets/education_logo/ignou.png"
+import tenthLogo from "./assets/education_logo/10.png"
+import masterLogo from "./assets/education_logo/master.png"
+import zidioLogo from "./assets/company_logo/zidioLogo.webp"
+
+export const SkillsInfo = [
+  {
+    title: 'Frontend',
+    skills: [
+      { name: 'HTML', logo: htmlLogo },
+      { name: 'CSS', logo: cssLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'React JS', logo: reactjsLogo },
+      { name: 'Redux', logo: reduxLogo },
+      { name: 'Next JS', logo: nextjsLogo },
+      { name: 'Tailwind CSS', logo: tailwindcssLogo },
+      { name: 'Material Ui', logo: materialuiLogo },
+      { name: 'Bootstrap', logo: bootstrapLogo },
+      
+    ],
+  },
+  {
+    title: 'Backend',
+    skills: [
+      { name: 'Node JS', logo: nodejsLogo },
+      { name: 'Express JS', logo: expressjsLogo },
+      { name: 'MongoDB', logo: mongodbLogo },
+      { name: 'Cloudinary', logo: cloudinaryLogo }, 
+      { name: 'JWT', logo: jwtLogo },
+      { name: 'Bcrypt', logo: bcryptLogo },
+      { name: 'Argon2', logo: argonLogo },
+      { name: 'Nodemailer', logo: NodemailerLogo },
+
+    ],
+  },
+  {
+    title: 'Languages',
+    skills: [
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'TypeScript', logo: typescriptLogo },
+    ],
+  },
+  {
+    title: 'Tools',
+    skills: [
+      { name: 'Git', logo: gitLogo },
+      { name: 'GitHub', logo: githubLogo },
+      { name: 'VS Code', logo: vscodeLogo },
+      { name: 'Postman', logo: postmanLogo },
+      { name: 'Compass', logo: mcLogo },
+      { name: 'Vercel', logo: vercelLogo },
+    ],
+  },
+];
+
+  export const experiences = [
+    {
+      id: 2,
+      img: ashatech,
+      role: "Full Stack Developer - Intern",
+      company: "Asha Tech",
+      date: "July 2025 - Present",
+      desc: "Worked as a full-stack developer on multi-modal and e-commerce projects, optimizing and building scalable UI components and web applications with React, Node.js, Express, MongoDB, and microservices to deliver responsive, high-performance sites.",
+      skills: [
+        "HTML",
+        "CSS",
+        "Tailwind CSS",
+        "Javascript",
+        "React.js",
+        "Next.js",
+        "Redux",
+        "Framer Motion",
+        "Git",
+        "GitHub",
+        "REST APIs",
+        "Postman",
+      ],
+    },
+    {
+      id: 0,
+      img: zidioLogo,
+      role: "Fullstack Developer Intern Remote",
+      company: "Zidio",
+      date: "3 Months",
+      desc: "Developed dynamic and scalable web applications using the MERN stack, handling both frontend and backend development. Collaborated with cross-functional teams to build responsive UI, implement RESTful APIs, and optimize application performance in an agile environment.",
+      skills: [
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "React JS",
+        "Node JS",
+        "Tailwind CSS",
+        "MongoDb",
+        "Redux",
+      ],
+    },
+  
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: masterLogo,
+      school: "IGNOU , Delhi",
+      date: "Persuing",
+      grade: "*",
+      desc: "I am pursuing my Master's degree (MCA) in Computer Applications from IGNOU, Delhi.",
+      degree: "Master of Computer Applications - MCA",
+    },
+    {
+      id: 1,
+      img: ignouLogo,
+      school: "MERIT, Delhi",
+      date: "Sept 2021 - Aug 2024",
+      grade: "65.5%",
+      desc: "I completed my Bachelor's degree in Bachelor Of Computer Application (BCA) from IGNOU, Delhi.  Diving into Web Development and Database Management Systems, I gained practical insights into the world of software development.",
+      degree: "Bachelor of Computer Application - BCA ",
+    },
+    {
+      id: 2,
+      img: schoolLogo,
+      school: "PremChand High School Sarari, Siwan",
+      date: "Apr 2019 - March 2021",
+      grade: "78%",
+      desc: "I completed my class 12 education from PremChand High School, Sarari, under the BSEB board, where I studied Physics, Chemistry, and Mathematics (PCM)",
+      degree: "BSEB (XII) - Physics, Chemistry, Math ",
+    },
+    {
+      id: 3,
+      img: tenthLogo,
+      school: "PremChand High School Sarari, Siwan",
+      date: "Apr 2018 - March 2019",
+      grade: "78%",
+      desc: "I completed my class 10 education from PremChand High School, Sarari, under the BSEB board, where I studied Science with Math.",
+      degree: "BSEB (X), Science with Math",
+    },
+  ];
+  
+  export const projects = [
+        {
+      id: 1,
+      title: "FireCommerce",
+      description:
+        "FireCommerce is a fashion e-commerce platform offering trendy apparel and accessories, featuring seasonal collections, bestsellers, and curated categories. The store delivers a modern shopping experience with sleek design, product showcases",
+      image: fireCommerce,
+      tags: ["Next JS","Tailwind", "JavaScript","Node Js","MongoDB","Express Js"],
+      github: "https://github.com/NoorAzam-1",
+      webapp: "https://firecomm.vercel.app/",
+    },
+    {
+      id: 2,
+      title: "Excel Analytics",
+      description:
+        "Upload, visualize, and analyze your Excel files with powerful, AI-driven tools — all in one intuitive and beautiful app.",
+      image: excelAnalytics,
+      tags: ["React Js", "API","Tailwind", "JavaScript","Node.js", "MongoDB", "Express"],
+      github: "https://github.com/NoorAzam-1",
+      webapp: "https://excel-analytics-rouge.vercel.app/",
+    },
+    {
+      id: 3,
+      title: "Asha Learnology",
+      description:
+        "Asha Learnology is a Delhi-based tech training platform offering hands-on courses in AI, ML, web & mobile development, and data science & analytics—designed to upskill learners and boost career growth.",
+      image: AshaLearnology,
+      tags: ["React JS", "Node.js","Tailwind","Framer Motion","Node.js", "MongoDB", "Express"],
+      github: "https://github.com/NoorAzam-1",
+      webapp: "https://ashalearnology-vite.vercel.app/",
+    },
+    {
+      id: 4,
+      title: "AshaTech",
+      description:
+        "AshaTech is a Delhi NCR based digital agency crafting sleek web and mobile solutions—plus UI/UX design, SEO, and strategic IT consulting—to help businesses level up in the online world.",
+      image: AshaTech,
+      tags: ["React JS", "Node.js","Tailwind","Framer Motion","Node.js", "MongoDB", "Express"],
+      github: "https://github.com/NoorAzam-1",
+      webapp: "https://ashatech.co.in/",
+    },
+    {
+      id: 5,
+      title: "Wave Visas",
+      description:
+        "Wave Visas is a Delhi-based immigration agency making global dreams happen. They handle PR and visa applications—for countries like Canada, UK, Australia, and more—with expert help on resumes, documents, job search, and post-landing support",
+      image: WaveVisas,
+      tags: ["React JS", "Node.js","Tailwind","Framer Motion","Node.js", "MongoDB", "Express", "JavaScript", "Framer Motion"],
+      github: "https://github.com/NoorAzam-1",
+      webapp: "https://wave-visas.vercel.app/", 
+    },
+        {
+      id: 8,
+      title: "BlogPulse",
+      description:
+        "BlogPulse is your platform for sharing stories, insights, and ideas. We provide a clean, simple, and powerful space for writers and readers to connect.",
+          image: blogpulse,
+      tags: ["React js", "Tailwind", "Javascript", "Express", "MongoDB", "Nodejs", "REST APIs"],
+          github: "https://github.com/NoorAzam-1",
+          webapp: "https://blog-pulse-delta.vercel.app/",
+    },
+  ];  
